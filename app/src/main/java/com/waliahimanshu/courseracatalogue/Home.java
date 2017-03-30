017c4a20047e9bb119c891842b82d024b43dc35e
waliahimanshu <himanshu.walia@gmail.com>
2017-03-30 04:54:27 +0100

    jdk and build tools update

--- app/src/main/java/com/waliahimanshu/courseracatalogue/Home.java
+++ app/src/main/java/com/waliahimanshu/courseracatalogue/Home.java
@@ -1,13 +1,42 @@
 package com.waliahimanshu.courseracatalogue;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        View viewById = (TextView) findViewById(R.id.editText);
+        assert viewById != null;
+
+        /**
+         * method 3 via xml layout
+         */
+
+        /**
+         * method 2
+         */
+        viewById.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    /**
+     * method one
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+
     }
 }
