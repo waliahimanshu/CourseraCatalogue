c19922d422e602e25c9e2eaac6a3053a2c7fe5e5
waliahimanshu <himanshu.walia@gmail.com>
2017-05-13 16:28:41 +0100

    strict mode in Android

--- app/src/main/java/com/waliahimanshu/courseracatalogue/CourseraCatalogueApplication.java
+++ app/src/main/java/com/waliahimanshu/courseracatalogue/CourseraCatalogueApplication.java
@@ -1,10 +1,29 @@
 package com.waliahimanshu.courseracatalogue;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 public class CourseraCatalogueApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (BuildConfig.DEBUG)
+            // Tell Android what thread issues you want to detect and what to do when found.
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .penaltyLog()
+                    .penaltyDialog()
+                    .build());
+
+        // Tell Android what VM issues you want to detect and what to do when found.
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()       // Log the problem
+                .penaltyDeath()     // Then kill the app
+                .build());
     }
 }
